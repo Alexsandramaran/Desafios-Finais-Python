809f93e8bcd84205308bce0f1bc91c92b1fd3bd7
Alexsandra Maran <78243487+Alexsandramaran@users.noreply.github.com>
2022-03-31 14:41:36 -0300

    Cognizant Cloud Data Engineer #2 PYTHON
    Desafios Finais Python - Cognizant Data Cloud Engineer #2

---
+++ Desafios-Finais-Python/Encaixa ou não.py
@@ -0,0 +1,10 @@
+test_cases = int(input())
+
+for i in range(test_cases):
+    line = input()
+    number_a = line.split()[0]
+    number_b = line.split()[1]
+    if (number_b != number_a[len(number_a)-len(number_b):] or len(number_b)>len(number_a)):
+        print('nao encaixa')
+        continue
+    print('encaixa')
